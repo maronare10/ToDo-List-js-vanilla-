c38b5dd98eab2e506ed8b86f83b5248a5807f8f1
Mariana Navarro <maronare10@gmail.com>
2025-07-29 18:01:52 -0500

    usando el DOM, capturando en  el html

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const formulario = document.getElementById('formulario')
+const input = document.getElementById('input')
 const listaTarea = document.getElementById('lista-tareas')
-const template = document.getElementById('template')
+const template = document.getElementById('template').content
 const fragment = document.createDocumentFragment()
 let tareas = {
-  
+  1753828069536: {
+    id: 1753828069536,
+    texto: 'Tarea #1',
+    estado: false
+  },
+  1753828247641: {
+    id: 1753828247641,
+    texto: 'Tarea #2',
+    estado: false
+  }  
 }
+
+//console.log(Date.now())
+
+formulario.addEventListener('submit', e=> {
+  e.preventDefault()
+  console.log(e.target[0].value);
+  console.log(e.target.querySelector('input').value);
+  console.log(input.value);
+  
+  
+  
+})
