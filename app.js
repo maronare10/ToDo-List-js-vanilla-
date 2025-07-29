5978708c0d28d3603f1ddb6843d8d717674d5be4
Mariana Navarro <maronare10@gmail.com>
2025-07-29 17:19:47 -0500

    aplicando js

---
+++ app.js
@@ -0,0 +1,7 @@
+const formulario = document.getElementById('formulario')
+const listaTarea = document.getElementById('lista-tareas')
+const template = document.getElementById('template')
+const fragment = document.createDocumentFragment()
+let tareas = {
+  
+}
